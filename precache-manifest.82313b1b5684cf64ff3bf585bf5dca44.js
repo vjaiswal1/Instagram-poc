62b749b7c81843031a10f93949dfe68f41592c79
Vikas Jaiswal <vikasjaiswal11@gmail.com>
2020-07-31 20:08:39 +0530

    Updates

---
+++ precache-manifest.82313b1b5684cf64ff3bf585bf5dca44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "523c780a4d63617c689eb07d673528a2",
+    "url": "/hello-reacts/index.html"
+  },
+  {
+    "revision": "b2bd4135261f70c48d70",
+    "url": "/hello-reacts/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "aa2aa0800e68bdc8fac6",
+    "url": "/hello-reacts/static/css/main.308dc0cd.chunk.css"
+  },
+  {
+    "revision": "b2bd4135261f70c48d70",
+    "url": "/hello-reacts/static/js/2.9328ba90.chunk.js"
+  },
+  {
+    "revision": "32b936c81a941aaedad03872d7767253",
+    "url": "/hello-reacts/static/js/2.9328ba90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa2aa0800e68bdc8fac6",
+    "url": "/hello-reacts/static/js/main.6fe24812.chunk.js"
+  },
+  {
+    "revision": "4e541751038c3b7fc944",
+    "url": "/hello-reacts/static/js/runtime-main.40d24ab6.js"
+  },
+  {
+    "revision": "9f4e94dfd08d040ad3d7f239e4bccd96",
+    "url": "/hello-reacts/static/media/saloon1.9f4e94df.jpg"
+  }
+]);
